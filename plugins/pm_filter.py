64ea8ef6e1136813de7dea14e760b88451c20f9c
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-02-13 13:41:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -369,7 +369,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer("Join the group man! 😒", show_alert=True)
+            await query.answer("Join our Back-up channel mahn! 😒", show_alert=True)
             return
         ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
