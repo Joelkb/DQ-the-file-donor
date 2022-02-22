a992f0e12c6e3eddb730b39158d91ac2905ddd53
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-02-22 22:09:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -41,7 +41,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(ALRT_TXT.format(query.from_user.first_name, query.message.reply_to_message.from_user.first_name), show_alert=True)
+        return await query.answer(ALRT_TXT.format(query.from_user.first_name), show_alert=True)
     try:
         offset = int(offset)
     except:
