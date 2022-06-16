b8cf990b8e7f25d745628638f2f3fd99f05c40e7
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-06-16 19:41:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -4,7 +4,7 @@ import re
 import ast
 
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from Script import script, ALRT_TXT, OLD_ALRT_TXT, CUDNT_FND, I_CUDNT, I_CUD_NT, MVE_NT_FND, TOP_ALRT_MSG, OWNER_INFO, URL
+from Script import script, ALRT_TXT, OLD_ALRT_TXT, CUDNT_FND, I_CUDNT, I_CUD_NT, MVE_NT_FND, TOP_ALRT_MSG, OWNER_INFO
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
@@ -814,7 +814,7 @@ async def advantage_spell_chok(msg):
             text=(I_CUD_NT.format(query)),
             reply_markup = InlineKeyboardMarkup(
              [[
-                InlineKeyboardButton("Check Spelling On Google ✅", url=(URL.format(query)))
+                InlineKeyboardButton("Check Spelling On Google ✅", url=f'https://google.com/search?q={query}')
              ]]
             )
         )
