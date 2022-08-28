24fd18d9a5ef358b4ed82356f0a6043c9fc4d49b
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-08-28 10:55:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -912,7 +912,12 @@ async def manual_filters(client, message, text=False):
                 try:
                     if fileid == "None":
                         if btn == "[]":
-                            await client.send_message(group_id, reply_text, disable_web_page_preview=True)
+                            await client.send_message(
+                                group_id, 
+                                reply_text, 
+                                disable_web_page_preview=True,
+                                reply_to_message_id=reply_id
+                            )
 
                         else:
                             button = eval(btn)
