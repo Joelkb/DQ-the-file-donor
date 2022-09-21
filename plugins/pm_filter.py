517fc9aa02502a417d5304e9c0ed9318bb4c8a44
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-09-21 11:14:51 +0530

    Trying to fix a bug

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -35,8 +35,15 @@ SPELL_CHECK = {}
 async def give_filter(client, message):
     settings = await get_settings(message.chat.id)
     await manual_filters(client, message)
-    if settings['auto_ffilter']:
-        await auto_filter(client, message)
+    try:
+        if settings['auto_ffilter']:
+            await auto_filter(client, message)
+    except KeyError:
+        grpid = await active_connection(str(message.from_user.id))
+        await save_group_settings(grpid, 'auto_ffilter', True)
+        settings = await get_settings(message.chat.id)
+        if settings['auto_ffilter']:
+            await auto_filter(client, message)
 
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
