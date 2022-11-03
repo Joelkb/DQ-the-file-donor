98652f4bfa5ce1b9f728a66b1ea7b6cbcfa48cc2
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-11-03 22:14:13 +0530

    Forget to import 😅

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -7,6 +7,7 @@ from database.ia_filterdb import Media
 from utils import get_size, temp, get_settings
 from Script import script
 from pyrogram.errors import ChatAdminRequired
+import asyncio 
 
 """-----------------------------------------https://t.me/GetTGLink/4179 --------------------------------------"""
 
