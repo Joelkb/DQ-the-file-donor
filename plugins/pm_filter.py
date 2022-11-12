4e44cded7ab086f6abb09d59e681700bace87e28
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-11-12 21:33:16 +0530

    forget to import 😅

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -3,6 +3,7 @@ import asyncio
 import re
 import ast
 import math
+import random
 
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
