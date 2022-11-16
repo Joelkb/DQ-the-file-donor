41dcef06335eabf1d6046bdcb10629654f674cde
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-11-16 19:37:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1081,7 +1081,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     
 async def auto_filter(client, msg, spoll=False):
-    mv_rqst = msg.message.text
+    mv_rqst = message.text
     reqstr1 = msg.from_user.id if msg.from_user else 0
     reqstr = await client.get_users(reqstr1)
     if not spoll:
