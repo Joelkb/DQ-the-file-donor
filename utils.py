7badc88969448212e5249b74efe4f33c50d9c426
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-11-23 22:18:14 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -156,11 +156,10 @@ async def broadcast_messages(user_id, message):
 
 async def search_gagala(text):
     usr_agent = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/61.0.3163.100 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0'
     }
     text = text.replace(" ", '+')
-    url = f'https://www.bing.com/search?q={text}'
+    url = f'https://www.google.com/search?q={text}'
     response = requests.get(url, headers=usr_agent)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'html.parser')
