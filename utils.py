d2742a6fc4c7aac25c40d7f23b4b99a4f706ae63
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-11-23 21:53:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -155,9 +155,7 @@ async def broadcast_messages(user_id, message):
         return False, "Error"
 
 async def search_gagala(text):
-    usr_agent = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-    "AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/107.0.0.0 Safari/537.36",
+    usr_agent = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.5",
     "Accept-Encoding": "gzip, deflate",
