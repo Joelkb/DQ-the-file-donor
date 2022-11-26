541f2c715a3a542751b9173752ccd1fe8de6e506
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-11-26 21:56:09 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -58,8 +58,13 @@ async def grp_brodcst(bot, message):
         pti, sh = await broadcast_messages(int(chat['id']), b_msg)
         if pti:
             success += 1
-        else:
-            failed += 1
+        elif pti == False:
+            if sh == "Blocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
+                failed += 1
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
