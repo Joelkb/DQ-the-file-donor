d8d1f93ddb6d91718aad865e7860d6f2d1dbffbe
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-11-26 22:24:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -704,10 +704,6 @@ async def send_msg(bot, message):
             if cmd in target_id:
                 target_id = target_id.replace(cmd, "")
         success = False
-        chat_detail = await bot.get_chat(chat_id=target_id)
-        if chat_detail.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL]:
-            await message.reply_text("<b>You can't send messages to any channel or group !</b>")
-            return
         try:
             user = await bot.get_users(target_id)
             users = await db.get_all_users()
