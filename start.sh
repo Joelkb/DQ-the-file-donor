330914725d99715c3dd86e72fd8add2451c9cfaf
MOVIESWORLDBoy <117733454+MOVIESWORLDBoy@users.noreply.github.com>
2022-11-28 21:05:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/MOVIESWORLDBoy/DQ-the-file-donor.git /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-The-File-Donor
 fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting ROLEX...."
 python3 bot.py
