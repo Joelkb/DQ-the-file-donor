ddceb374f0ba7823bcdd0348a93453aac187797d
MOVIESWORLDBoy <117733454+MOVIESWORLDBoy@users.noreply.github.com>
2022-11-28 22:28:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MOVIESWORLDBoy/DQ-the-file-donor/tree/Private.git /DQ-the-file-donor/tree/Private
+  git clone https://github.com/MOVIESWORLDBoy/DQ-the-file-donor/tree/Private.git /DQ-the-file-donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-the-file-donor/tree/Private
+  git clone $UPSTREAM_REPO /DQ-the-file-donor
 fi
-cd /DQ-the-file-donor/tree/Private
+cd /DQ-the-file-donor
 pip3 install -U -r requirements.txt
 echo "Starting ROLEX...."
 python3 bot.py
