8bc54111f6e97181e5399042059aec5e97c9cbb2
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-12-02 19:37:24 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -156,21 +156,29 @@ async def broadcast_messages(user_id, message):
         return False, "Error"
 
 async def search_gagala(text):
-    usr_agent = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-        'Chrome/61.0.3163.100 Safari/537.36'
-        }
+    agent_list = [
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0',
+        'Opera/9.80 (Macintosh; Intel Mac OS X; U; en) Presto/2.2.15 Version/10.00',
+        'Opera/9.60 (Windows NT 6.0; U; en) Presto/2.1.1',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59'
+    ]
     text = text.replace(" ", '+')
     url = f'https://www.google.com/search?q={text}'
     retries = 1
     success = False
     while not success:
         try:
+            usr_agent = {
+                'User-Agent': random.choice(agent_list)
+                }
             response = requests.get(url, headers=usr_agent)
             success = True
         except Exception as e:
-            wait = retries * 30
-            print(f'Error: Wait for {wait} seconds.')
+            wait = retries * 10
+            print(f'Error: {e}\n\nWait for {wait} seconds to retry !')
             asyncio.sleep(wait)
             retries += 1
     soup = BeautifulSoup(response.text, 'html.parser')
