060f0ca225f352ba0397f1b3dbe641e34d4850d5
Localusersbot <115877356+Localusersbot@users.noreply.github.com>
2022-12-10 23:24:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,7 +12,7 @@ Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
 🧑‍💻 Developer : <a href='https://t.me/AFxSU'>🍒ɛӼ_ռöֆ__⁷⁷⁷ ✘ ⁴✨🦋</a>
 🎪 Data Base : <a href='https://www.mongodb.com/'>𝙼𝙾𝙽𝙶𝙾 𝙳𝙱</a>
 📡 Server : <a href='https://t.me/quickfastt'>QuickFast'</a>
-🔋 Bot Group : <a href='https://t.me/at3movies>MovieGroup'</a></b>"""
+🔋 Bot Group : <a href='https://t.me/at3movies'>MovieGroup</a> </b>"""
 
     SOURCE_TXT = """<b>ɴᴏᴛᴇ:
 - ⚠️ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ɴᴏᴛ ᴀɴ  ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ ⚠️.
