c40fc7b612e17fe789f3feb8e4a5df31fbaa7e58
Localusersbot <115877356+Localusersbot@users.noreply.github.com>
2022-12-12 08:29:25 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -39,7 +39,7 @@ async def save_group(bot, message):
             return
         buttons = [[
                      InlineKeyboardButton('ᴄʜᴇᴄᴋ ᴩᴍ', url=f"https://t.me/{temp.U_NAME}"), 
-                     InlineKeyboardButton("ʙᴏᴛ oᴡɴᴇʀ", url="t.me/AFxSU")
+                     InlineKeyboardButton("ʙᴏᴛ oᴡɴᴇʀ", url=f"https://t.me/aboutexinos")
                  ],[
                      InlineKeyboardButton(" ʀᴇᴀᴅ ɢʀᴏᴜᴘ ʀᴜʟᴇs 📌", url="https://graph.org/%F0%9D%90%91%F0%9D%90%9A%F0%9D%90%AC%F0%9D%90%A1%F0%9D%90%A2%F0%9D%90%A6%F0%9D%90%A2%F0%9D%90%A4%F0%9D%90%9A-11-26")
                   ]] 
@@ -62,7 +62,7 @@ async def save_group(bot, message):
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
                                                                             InlineKeyboardButton('ᴄʜᴇᴄᴋ ᴩᴍ', url=f"https://t.me/{temp.U_NAME}"), 
-                                                                            InlineKeyboardButton("ʙᴏᴛ oᴡɴᴇʀ", url="t.me/AFxSU")
+                                                                            InlineKeyboardButton("ʙᴏᴛ oᴡɴᴇʀ", url=f"https://t.me/aboutexinos")
                                                                         ],[
                                                                             InlineKeyboardButton(" ʀᴇᴀᴅ ɢʀᴏᴜᴘ ʀᴜʟᴇs 📌", url="https://graph.org/%F0%9D%90%91%F0%9D%90%9A%F0%9D%90%AC%F0%9D%90%A1%F0%9D%90%A2%F0%9D%90%A6%F0%9D%90%A2%F0%9D%90%A4%F0%9D%90%9A-11-26")
                                                                         ]] 
