2c03d395a66ab01cd540378a8e337ddf4df98af8
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-12-17 09:29:17 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -8,7 +8,7 @@ from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
 from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN
-from utils import get_settings
+from utils import get_settings, save_group_settings
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -70,10 +70,18 @@ async def save_file(media):
 async def get_search_results(chat_id, query, file_type=None, max_results=10, offset=0, filter=False):
     """For given query return (results, next_offset)"""
     settings = await get_settings(int(chat_id))
-    if settings['max_btn']:
-        max_results = 10
-    else:
-        max_results = int(MAX_B_TN)
+    try:
+        if settings['max_btn']:
+            max_results = 10
+        else:
+            max_results = int(MAX_B_TN)
+    except KeyError:
+        await save_group_settings(int(chat_id), 'max_btn', False)
+        settings = await get_settings(int(chat_id))
+        if settings['max_btn']:
+            max_results = 10
+        else:
+            max_results = int(MAX_B_TN)
     query = query.strip()
     #if filter:
         #better ?
