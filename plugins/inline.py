05cb0d95949d64c3b22a85a3e598dbeb992ee008
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-12-17 19:25:00 +0530

    Fixed Inline Search Not Working....

--- plugins/inline.py
+++ plugins/inline.py
@@ -5,6 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQue
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from database.connections_mdb import active_connection
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
@@ -22,6 +23,7 @@ async def inline_users(query: InlineQuery):
 @Client.on_inline_query()
 async def answer(bot, query):
     """Show search results for given inline query"""
+    chat_id = await active_connection(str(query.from_user.id))
     
     if not await inline_users(query):
         await query.answer(results=[],
@@ -48,7 +50,9 @@ async def answer(bot, query):
 
     offset = int(query.offset or 0)
     reply_markup = get_reply_markup(query=string)
-    files, next_offset, total = await get_search_results(string,
+    files, next_offset, total = await get_search_results(
+                                                  chat_id,
+                                                  string,
                                                   file_type=file_type,
                                                   max_results=10,
                                                   offset=offset)
