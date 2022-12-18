314b510e1e807b1daf31d80f8d44feec164538d2
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-12-18 22:09:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1538,6 +1538,7 @@ async def advantage_spell_chok(client, msg):
         await k.delete()
         return
     movielist += [movie.get('title') for movie in movies]
+    movielist += [f"{movie.get('title')} {movie.get('year')}" for movie in movies]
     SPELL_CHECK[mv_id] = movielist
     btn = [
         [
