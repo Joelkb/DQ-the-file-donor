397465104b215674be39f84edb3b8b761d34eeee
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2023-01-23 17:41:03 +0530

    Update in file deletion

--- plugins/commands.py
+++ plugins/commands.py
@@ -730,19 +730,21 @@ async def send_msg(bot, message):
 @Client.on_message(filters.command("deletefiles") & filters.user(ADMINS))
 async def deletemultiplefiles(bot, message):
     chat_type = message.chat.type
-    if chat_type != enums.ChatType.PRIVATE: #No need to use this command on groups
+    if chat_type != enums.ChatType.PRIVATE:
         return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command won't work in groups. It only works on my PM !</b>")
     else:
         pass
     try:
-        keyword = message.text.split(" ", 1)[1] #extracting keyword from command
-    except: #if extracting failed
+        keyword = message.text.split(" ", 1)[1]
+    except:
         return await message.reply_text(f"<b>Hey {message.from_user.mention}, Give me a keyword along with the command to delete files.</b>")
     k = await bot.send_message(chat_id=message.chat.id, text=f"<b>Fetching Files for your query {keyword} on DB... Please wait...</b>")
-    files, next_offset, total = await get_bad_files(keyword) #fetching files from db
-    await k.edit_text(f"<b>Found {total} files for your query {keyword} !</b>")
+    files, next_offset, total = await get_bad_files(keyword)
+    await k.edit_text(f"<b>Found {total} files for your query {keyword} !\n\nFile deletion process will start in 5 seconds !</b>")
+    await asyncio.sleep(5)
     deleted = 0
     for file in files:
+        await k.edit_text(f"<b>Process started for deleting files from DB. Successfully deleted {str(deleted)} files from DB for your query {keyword} !\n\nPlease wait...</b>")
         file_ids = file.file_id
         file_name = file.file_name
         result = await Media.collection.delete_one({
@@ -751,5 +753,4 @@ async def deletemultiplefiles(bot, message):
         if result.deleted_count:
             logger.info(f'File Found for your query {keyword}! Successfully deleted {file_name} from database.')
         deleted += 1
-    deleted = str(deleted)
-    await k.edit_text(text=f"<b>Successfully deleted {deleted} files from database for your query {keyword}.</b>")
+    await k.edit_text(text=f"<b>Process Completed for file deletion !\n\nSuccessfully deleted {str(deleted)} files from database for your query {keyword}.</b>")
