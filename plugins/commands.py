9b8b2f7f934bf9c266126826f568eaf71ca3aa62
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2023-01-23 17:43:58 +0530

    Small change

--- plugins/commands.py
+++ plugins/commands.py
@@ -754,7 +754,8 @@ async def deletemultiplefiles(bot, message):
         return await message.reply_text(f"<b>Hey {message.from_user.mention}, Give me a keyword along with the command to delete files.</b>")
     k = await bot.send_message(chat_id=message.chat.id, text=f"<b>Fetching Files for your query {keyword} on DB... Please wait...</b>")
     files, next_offset, total = await get_bad_files(keyword)
-    await k.edit_text(f"<b>Found {total} files for your query {keyword} !</b>")
+    await k.edit_text(f"<b>Found {total} files for your query {keyword} !\n\nFile deletion process will start in 5 seconds !</b>")
+    await asyncio.sleep(5)
     deleted = 0
     for file in files:
         await k.edit_text(f"<b>Process started for deleting files from DB. Successfully deleted {str(deleted)} files from DB for your query {keyword} !\n\nPlease wait...</b>")
