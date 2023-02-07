d906ba46baf33661493d7d74281cd53ffcff8b24
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-07 17:52:47 +0530

    fix banned member status

--- utils.py
+++ utils.py
@@ -49,7 +49,7 @@ async def is_subscribed(bot, query):
     except Exception as e:
         logger.exception(e)
     else:
-        if user.status != 'kicked':
+        if user.status != enums.ChatMemberStatus.BANNED:
             return True
 
     return False
