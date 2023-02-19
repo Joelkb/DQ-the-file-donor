304b401841ed01da95db5fe0c28118b115684ff6
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-02-19 12:01:25 +0530

    Update carbon.py

--- plugins/ExtraMods/carbon.py
+++ plugins/ExtraMods/carbon.py
@@ -42,8 +42,8 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**Made by @mkn_bots_updates**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/mkn_bots_updates")]]),                   
+        caption="**ᴛʜɪs ᴘɪᴄ ɪs ɴɪᴄᴇ ᴏɴᴇ ᴍᴀᴅᴇ ʙʏ ᴍʟᴢ ʙᴏᴛᴢ​**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ​ 🧩", url="https://t.me/MLZ_BOTZ_SUPPORT")]]),                   
     )
     await m.delete()
     carbon.close()
