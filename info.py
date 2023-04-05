315812dbe2a0310b3acc08b2a1f1fbdea25c7ee2
HDCinema4U <115459136+HDCinema4U@users.noreply.github.com>
2023-04-05 12:59:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,8 +42,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Baki:Baki@cluster0.svipuby.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Baki")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://King:king@cluster0.cwqhf0m.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "King")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
