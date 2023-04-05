33f8d8f7cad95702a3679ad131581c6ba66ae11b
HDCinema4U <115459136+HDCinema4U@users.noreply.github.com>
2023-04-05 13:56:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,8 +42,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Naruto:Naruto@cluster0.ocgctrg.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Naruto")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Max:Max@max.sfxacmv.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Max")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
