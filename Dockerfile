90bf018c42772e1097989dd7b1b21c449d764655
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2023-05-02 00:14:16 +0530

    Fixed UPSTREAM_REPO
    Fixed UPSTREAM_REPO not working problem on heroku when using main branch. Fixed Dockerfile. Now you can UPSTREAM_REPO

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,7 @@ COPY requirements.txt /requirements.txt
 
 RUN cd /
 RUN pip3 install -U pip && pip3 install -U -r requirements.txt
-WORKDIR /DQ-The-File-Donor
-
-COPY . .
-
-CMD ["python3", "bot.py"]
+RUN mkdir /DQTheFileDonor
+WORKDIR /DQTheFileDonor
+COPY start.sh /start.sh
+CMD ["/bin/bash", "/start.sh"]
--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /DQTheFileDonorBot
 fi
-cd /DQ-The-File-Donor
+cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
