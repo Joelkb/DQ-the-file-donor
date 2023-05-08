103da46a3ba8941a6244c8b7579bd6f04ef9d5ee
1238muj <122881477+1238muj@users.noreply.github.com>
2023-05-08 11:29:16 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/1238muj/DQ-the-file-donor
+  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
