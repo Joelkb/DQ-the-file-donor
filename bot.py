193ee207479ea715e12f64adfe2266a3b3441f4f
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2023-05-09 20:56:04 +0530

    Just for testing purposes

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
+            workers=100,
             plugins={"root": "plugins"},
             sleep_threshold=5,
         )
