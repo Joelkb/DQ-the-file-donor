71282eabd66717883a4e7808125bbb2c1b7863a2
Susil2510 <132734591+Susil2510@users.noreply.github.com>
2023-05-10 20:02:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1275,7 +1275,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/creatorbeatz")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/Susil25_movie")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
