a5a569a13a0d870274e7d5c50e2a420568af150b
Susil2510 <132734591+Susil2510@users.noreply.github.com>
2023-05-10 20:23:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -182,7 +182,7 @@ async def start(client, message):
                           InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                           InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                        ],[
-                          InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
+                          InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Susil25_movie")
                          ]
                         ]
                     )
