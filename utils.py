d8743a9ff3c2a4d0a4b58db8934a2e8a4747b270
Susil2510 <132734591+Susil2510@users.noreply.github.com>
2023-05-16 13:48:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -646,7 +646,7 @@ async def send_all(bot, userid, files, ident):
                         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                     ],[
-                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
+                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Susil25_movie")
                         ]
                     ]
                 )
