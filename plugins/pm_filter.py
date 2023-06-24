635a0c8dba617b6ba9bd4605f7aa2d61d896d763
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2023-06-24 14:19:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1197,15 +1197,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
     
     elif query.data == "hero":
         buttons = [[
-            InlineKeyboardButton('ನಟಸಾರ್ವಬೌಮ ಡಾ. ರಾಜ್‌ಕುಮಾರ್', callback_data='rajkumar')
+            InlineKeyboardButton('R.   ನಟಸಾರ್ವಬೌಮ ಡಾ. ರಾಜ್‌ಕುಮಾರ್', callback_data='rajkumar')
         ], [
-            InlineKeyboardButton('ಸಾಹಸ ಸಿಂಹ ಡಾ. ವಿಷ್ಣುವರ್ಧನ್', callback_data='vishnuvardhan')
+            InlineKeyboardButton('V.      ಸಾಹಸ ಸಿಂಹ ಡಾ. ವಿಷ್ಣುವರ್ಧನ್', callback_data='vishnuvardhan')
         ], [
-            InlineKeyboardButton('ರೆಬೆಲ್ ಸ್ಟಾರ್ ಅಂಬರೀಷ್', callback_data='ambarish')
+            InlineKeyboardButton('A.           ರೆಬೆಲ್ ಸ್ಟಾರ್ ಅಂಬರೀಷ್', callback_data='ambarish')
         ], [
-            InlineKeyboardButton('ಕರಾಟೆ ಕಿಂಗ್ ಶಂಕರ್ ನಾಗ್', callback_data='shankarnag')
+            InlineKeyboardButton('S.         nಕರಾಟೆ ಕಿಂಗ್ ಶಂಕರ್ ನಾಗ್', callback_data='shankarnag')
         ], [
-            InlineKeyboardButton('ಪವರ್ ಸ್ಟಾರ್ ಪುನೀತ್ ರಾಜ್‌ಕುಮಾರ್', callback_data='punithrajkumar')
+            InlineKeyboardButton('P.   ಪವರ್ ಸ್ಟಾರ್ ಪುನೀತ್ ರಾಜ್‌ಕುಮಾರ್', callback_data='punithrajkumar')
         ], [
             InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
             InlineKeyboardButton('Next', callback_data='hero1')
