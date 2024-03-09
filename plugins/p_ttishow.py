c9c9d6e33fdf172bd5249671923da3ead81cd43b
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2024-03-09 22:21:22 +0530

    Sry forget to import

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, MELCOW_NEW_USERS, MELCOW_VID, CHNL_LNK, GRP_LNK
 from database.users_chats_db import db
-from database.ia_filterdb import Media
+from database.ia_filterdb import Media, db as clientDB
 from utils import get_size, temp, get_settings
 from Script import script
 from pyrogram.errors import ChatAdminRequired
