64059068a3a3d2c4ad73d678613fd2505cdb5ce3
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2024-03-10 18:31:11 +0530

    Fixed Search results not showing queries with year in parentheses.
    From now on, for queries like Evil Dead Rise 2023 or Evil Dead Rise (2023), files having names like Evil Dead Rise (2023) will be shown in the search results. (My English is bad, so I'm assuming that you got what I meant)

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -93,7 +93,7 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
     elif ' ' not in query:
         raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_()]')
     
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
